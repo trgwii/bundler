e88c8965c3250b75f41fecf7042bb9e100394a3c
Thomas Rory Gummerson <thomas@gummerson.no>
2020-09-14 22:42:51 +0200

    Bugfixes in readerFromBuffer

--- readerFromBuffer.ts
+++ readerFromBuffer.ts
@@ -1,10 +1,17 @@
 export const readerFromBuffer = (data: Uint8Array): Deno.Reader => {
   let bytesWritten = 0;
   const read = async (out: Uint8Array) => {
-    const slice = data.subarray(bytesWritten, bytesWritten + out.byteLength);
-    const bytes = slice.byteLength;
+    if (bytesWritten >= data.byteLength) {
+      return null;
+    }
+    const bytes = Math.min(data.byteLength - bytesWritten, out.byteLength);
+    const slice = data.subarray(
+      bytesWritten,
+      bytesWritten + bytes,
+    );
     out.set(slice, 0);
-    return Promise.resolve(bytes);
+    bytesWritten += bytes;
+    return bytes;
   };
   return { read };
 };
