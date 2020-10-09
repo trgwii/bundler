90ffbb43b9c50e5d90cb53ac591eaf2e5042868d
Thomas Rory Gummerson <thomas@gummerson.no>
2020-10-09 14:52:24 +0200

    Update all deps

--- deps.ts
+++ deps.ts
@@ -1,7 +1,7 @@
-export { encode } from "https://deno.land/std@0.70.0/encoding/ascii85.ts";
+export { encode } from "https://deno.land/std@0.74.0/encoding/ascii85.ts";
 export {
   readVarnum,
   writeVarnum,
-} from "https://deno.land/std@0.70.0/encoding/binary.ts";
-export { copyN } from "https://deno.land/std@0.70.0/io/ioutil.ts";
+} from "https://deno.land/std@0.74.0/encoding/binary.ts";
+export { copyN } from "https://deno.land/std@0.74.0/io/ioutil.ts";
 export { compress } from "https://deno.land/x/brotli@v0.1.4/mod.ts";
