e5a8ba604490aa534205fed741210abd9d3241d4
Thomas Rory Gummerson <thomas@gummerson.no>
2021-03-04 11:11:59 +0100

    update deps

--- deps.ts
+++ deps.ts
@@ -1,15 +1,15 @@
-export { encode } from "https://deno.land/std@0.88.0/encoding/ascii85.ts";
+export { encode } from "https://deno.land/std@0.89.0/encoding/ascii85.ts";
 export {
   readVarbig,
   readVarnum,
   writeVarbig,
   writeVarnum,
-} from "https://deno.land/std@0.88.0/encoding/binary.ts";
-export { copyN } from "https://deno.land/std@0.88.0/io/ioutil.ts";
+} from "https://deno.land/std@0.89.0/encoding/binary.ts";
+export { copyN } from "https://deno.land/std@0.89.0/io/ioutil.ts";
 export { compress } from "https://deno.land/x/brotli@v0.1.4/mod.ts";
 
 export const strings = {
-  decode: "https://deno.land/std@0.88.0/encoding/ascii85.ts",
+  decode: "https://deno.land/std@0.89.0/encoding/ascii85.ts",
   decompress: "https://deno.land/x/brotli@v0.1.4/mod.ts",
   parse: "https://raw.githubusercontent.com/trgwii/bundler/master/parse.ts",
 };
